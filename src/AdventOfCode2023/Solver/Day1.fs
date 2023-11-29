f773fab68d3c5cdd8a42e91e93b17e9ea508d7d5
Martin Thaller <martin.thaller@uipath.com>
2023-11-29 15:39:53 +0100

    init with day 1 from 2022

---
+++ src/AdventOfCode2023/Solver/Day1.fs
@@ -0,0 +1,19 @@
+﻿module AdventOfCode2023.Solver.Day1
+
+let sumInventories =
+    Array.fold
+        (fun (state: List<int>) el ->
+            match el with
+            | "" -> 0 :: state
+            | num -> (state.Head + (num |> int)) :: state.Tail)
+        (0 :: List.empty)
+
+let solver1 (lines: string array) =
+    lines |> sumInventories |> List.max
+
+let solver2 (lines: string array) =
+    lines
+    |> sumInventories
+    |> List.sortDescending
+    |> List.take 3
+    |> List.sum
---
+++ test/AdventOfCode2023.Tests/Day1Tests.fs
@@ -0,0 +1,32 @@
+﻿module AdventOfCode2023.Tests.Day1Tests
+
+open AdventOfCode2023.Solver
+open Xunit
+
+type Day1Tests() =
+    let demoData =
+        [| "1000"
+           "2000"
+           "3000"
+           ""
+           "4000"
+           ""
+           "5000"
+           "6000"
+           ""
+           "7000"
+           "8000"
+           "9000"
+           ""
+           "10000"
+           |]
+
+    [<Fact>]
+    let ``Day 1 part 1`` () =
+        let solution = Day1.solver1 demoData
+        Assert.Equal(24000, solution)
+
+    [<Fact>]
+    let ``Day 1 part 2`` () =
+        let solution = Day1.solver2 demoData
+        Assert.Equal(45000, solution)
