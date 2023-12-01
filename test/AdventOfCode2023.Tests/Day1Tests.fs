1f8b525f128dcb3d002fe23f32fee7895315fa53
Martin Thaller <martin.thaller@uipath.com>
2023-12-01 09:43:32 +0100

    day1

--- test/AdventOfCode2023.Tests/Day1Tests.fs
+++ test/AdventOfCode2023.Tests/Day1Tests.fs
@@ -5,28 +5,29 @@ open Xunit
 
 type Day1Tests() =
     let demoData =
-        [| "1000"
-           "2000"
-           "3000"
-           ""
-           "4000"
-           ""
-           "5000"
-           "6000"
-           ""
-           "7000"
-           "8000"
-           "9000"
-           ""
-           "10000"
+        [|
+            "1abc2"
+            "pqr3stu8vwx"
+            "a1b2c3d4e5f"
+            "treb7uchet"
            |]
 
+    let demoData2 =
+        [|
+            "two1nine"
+            "eightwothree"
+            "abcone2threexyz"
+            "xtwone3four"
+            "4nineeightseven2"
+            "zoneight234"
+            "7pqrstsixteen"
+        |]
     [<Fact>]
     let ``Day 1 part 1`` () =
         let solution = Day1.solver1 demoData
-        Assert.Equal(24000, solution)
+        Assert.Equal(142, solution)
 
     [<Fact>]
     let ``Day 1 part 2`` () =
-        let solution = Day1.solver2 demoData
-        Assert.Equal(45000, solution)
+        let solution = Day1.solver2 demoData2
+        Assert.Equal(281, solution)
