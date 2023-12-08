2b4ee703624c098faf9f465619f259a8f7ae307a
Martin Thaller <martin.thaller@uipath.com>
2023-12-08 15:14:50 +0100

    day 8 part 2

--- src/AdventOfCode2023/Solver/Day8.fs
+++ src/AdventOfCode2023/Solver/Day8.fs
@@ -2,32 +2,55 @@ module AdventOfCode2023.Solver.Day8
 
 open System.Collections.Generic
 
-type Node = { id: string; left: string; right: string }
+type Node =
+    { id: string
+      left: string
+      right: string }
 
-type MyMap = IDictionary<string,Node>
+type MyMap = IDictionary<string, Node>
 
 let getMap (lines: string array) =
     lines
     |> Array.map (fun x ->
         (x.Substring(0, 3),
-         {
-           id = x.Substring(0,3)
+         { id = x.Substring(0, 3)
            left = x.Substring(7, 3)
            right = x.Substring(12, 3) }))
     |> dict
 
-let rec findDestination (map:MyMap) (instructions: string) (node: Node) (count: int) : int =
-    if node.id = "ZZZ" then count
+let getNextNodeId instruction node =
+    match instruction with
+    | 'L' -> node.left
+    | 'R' -> node.right
+    | _ -> failwith "impossible"
+
+let rec findDistance (map: MyMap) (instructions: string) (node: Node) (count: int) endCondition : int =
+    if endCondition node.id then
+        count
     else
         let instruction = instructions[count % instructions.Length]
-        let nextNodeId = match instruction with
-                            | 'L' -> node.left
-                            | 'R' -> node.right
-                            | _ -> failwith "impossible"
-        findDestination map instructions map[nextNodeId] (count + 1)
+        let nextNodeId = getNextNodeId instruction node
+        findDistance map instructions map[nextNodeId] (count + 1) endCondition
 
 let solver1 (lines: string array) =
     let map = getMap (lines |> Array.skip 2)
-    findDestination map lines[0] map["AAA"] 0
+    findDistance map lines[0] map["AAA"] 0 (fun x -> x = "ZZZ")
 
-let solver2 (lines: string array) = failwith "error"
+let rec gcd a b =
+    match (a, b) with
+    | (x, y) when x = y -> x
+    | (x, y) when x > y -> gcd (x - y) y
+    | (x, y) -> gcd x (y - x)
+
+let lcm a b = a * b / (gcd a b)
+
+let solver2 (lines: string array) =
+    let map = getMap (lines |> Array.skip 2)
+    let startingNodes = map.Keys |> Seq.filter (fun x -> x[2] = 'A') |> Array.ofSeq
+
+    let distances =
+        startingNodes
+        |> Array.map (fun nodeId -> findDistance map lines[0] map[nodeId] 0 (fun x -> x[2] = 'Z'))
+        |> Array.map int64
+
+    distances |> Array.fold lcm 1
--- test/AdventOfCode2023.Tests/Day8Tests.fs
+++ test/AdventOfCode2023.Tests/Day8Tests.fs
@@ -13,6 +13,20 @@ type Day8Test() =
             "ZZZ = (ZZZ, ZZZ)"
         |]
 
+    let demoData2 =
+        [|
+            "LR"
+            ""
+            "11A = (11B, XXX)"
+            "11B = (XXX, 11Z)"
+            "11Z = (11B, XXX)"
+            "22A = (22B, XXX)"
+            "22B = (22C, 22C)"
+            "22C = (22Z, 22Z)"
+            "22Z = (22B, 22B)"
+            "XXX = (XXX, XXX)"
+        |]
+
     [<Fact>]
     let ``Day 8 part 1`` () =
         let solution = Day8.solver1 demoData
@@ -21,5 +35,5 @@ type Day8Test() =
 
     [<Fact>]
     let ``Day 8 part 2`` () =
-        let solution = Day8.solver2 demoData
-        Assert.Equal("", solution)
+        let solution = Day8.solver2 demoData2
+        Assert.Equal(6L, solution)
